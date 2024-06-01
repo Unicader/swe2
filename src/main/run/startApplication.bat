deb7ac0e5c350dbe7ae1899c5b325d5cb5741afc
Unicader <Marius.76228@web.de>
2024-06-01 21:12:39 +0200

    bat file works in bin folder

--- src/main/run/startApplication.bat
+++ src/main/run/startApplication.bat
@@ -1,4 +1,11 @@
 @echo off
 cd /d "../../.."
-javac -cp src/main/java;src/main/java/resources/gson-2.11.0.jar src/main/java/de/dhbw/cm/main/Main.java
-java -cp src/main/java;src/main/java/resources/gson-2.11.0.jar de.dhbw.cm.main.Main
+
+REM Define the directory for class files
+set CLASS_DIR=bin
+
+REM Compile the Java files and place the class files in the specified directory
+javac -d %CLASS_DIR% -cp src/main/java;src/main/java/resources/gson-2.11.0.jar src/main/java/de/dhbw/cm/main/Main.java
+
+REM Run the compiled Java program, specifying the class files directory in the classpath
+java -cp %CLASS_DIR%;src/main/java/resources/gson-2.11.0.jar de.dhbw.cm.main.Main
